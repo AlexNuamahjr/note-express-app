0f359719ab7c99faad643dc1f798dc4316402950
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-01 01:52:30 +0000

    joi package installed

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const helmet = require("helmet");
+const userRoutes = require("./routes/index.js");
 
 const app = express()
-    .use(helmet)
+    .use(helmet())
+    .use("/", userRoutes)
+    .use(express.json())
+    .use(express.urlencoded({extended: false}))
 
 
     module.exports = app;
