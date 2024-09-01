422851ee8dcbb37588d9f6e54336e43a72a1978a
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-01 01:51:43 +0000

    create new user

---
+++ controllers/index.js
@@ -0,0 +1,38 @@
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+
+const prisma = new PrismaClient();
+
+const createUser = async (req, res) => {
+  try {
+    const { firstName, lastName, userName, email, dateOfBirth, password } =
+      req.body;
+    // check if user exists
+    const isUserExists = await prisma.user.findFirst({where: {email: email}});
+    if (isUserExists){
+        return res.status(400).json({message: "User already exists"})
+    }
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);    
+    // create new user
+    const newUser = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        userName,
+        email,
+        dateOfBirth: new Date(dateOfBirth),
+        password: hashedPassword,
+      },
+    });
+    return res.status(201).json({message: "User created successfully", user: newUser});
+  } catch (error) {
+    return res.status(500).json({error: "Error occured while creating user"})
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+module.exports = {
+  createUser,
+};
