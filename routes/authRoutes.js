457440e8fac9190c8250f9399090488cefe59370
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-04 00:43:44 +0000

    auth routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const {forgotPassword, home} = require("../controllers/authController.js");
+const {forgotPasswordSchema} = require("../schemas/userSchema.js");
+const {validateRequest} = require("../middleware/requestValidate.js");
+
+const authRouter = express.Router();
+
+
+authRouter.post("/forgot-password", validateRequest(forgotPasswordSchema), forgotPassword)
+
+module.exports = authRouter;
