d2c856121b4fa4affd370a881d43ce4452c906bb
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-04 00:46:25 +0000

    update: express session

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const helmet = require("helmet");
+const expressSession = require("express-session");
 const userRoutes = require("./routes/index.js");
+const authRoutes = require("./routes/authRoutes.js");
 
 const app = express()
     .use(helmet())
     .use(express.json())
     .use(express.urlencoded({extended: false}))
+    .use(expressSession({
+        secret: process.env.APP_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {secure: false}
+    }))
     .use("/", userRoutes)
-
+    .use("/auth", authRoutes);
 
     module.exports = app;
