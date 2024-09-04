f26a00380f8cb156af68416d755a6d370539ab29
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-04 00:47:07 +0000

    nodemailer added

--- middleware/requestValidate.js
+++ middleware/requestValidate.js
@@ -6,7 +6,7 @@ const validateRequest = (schema)=>{
             
             const {error} = schema.validate(req.body, {abortEarly: false});
             if (error){
-                return res.status(400).json({error: error.details});
+                return res.status(400).json({ errors: error.details.map(err => err.message)});
             }
             next();
         } catch (error) {
