7ee9ae68650440446f11745cb468130617d791f4
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-10 21:08:40 +0000

    profile picture upload

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,30 @@ import { PrismaClient } from "@prisma/client";
 import { genertateToken } from "../utils/tokenGenerator.js";
 import bcrypt from "bcrypt";
 import nodemailer from "nodemailer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
 
 const prisma = new PrismaClient();
+
+// configure cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API,
+  api_secret: process.env.API_SECRET,
+});
+
+// set up cloudinary storage for multer
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "profile_pictures",
+    format: async (req, file) => "jpg",
+    public_id: (req, file) => "profle" + Date.now(),
+  },
+});
+
+const upload = multer({ storage: storage });
 
 // register user
 export const createUser = async (req, res) => {
@@ -29,7 +51,7 @@ export const createUser = async (req, res) => {
         email,
         dateOfBirth: new Date(dateOfBirth),
         password: hashedPassword,
-        isVerified: false
+        isVerified: false,
       },
     });
     // generate verification token
@@ -41,12 +63,12 @@ export const createUser = async (req, res) => {
       data: {
         token: hashedToken,
         userId: newUser.id,
-        expireAt: new Date(Date.now() + 3600000)
-      }
+        expireAt: new Date(Date.now() + 3600000),
+      },
     });
     // send token in email
     const verificationUrl = `http://localhost:8756/verify-email?token=${plainToken}&userId=${newUser.id}`;
-    
+
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
@@ -58,64 +80,66 @@ export const createUser = async (req, res) => {
       from: "no-reply@noteapp.com",
       to: email,
       subject: "Verify your email",
-      text: `Please verify your email by clicking the following link: ${verificationUrl}`
+      text: `Please verify your email by clicking the following link: ${verificationUrl}`,
     };
     await transporter.sendMail(mailOption);
-    
-    return res
-      .status(201)
-      .json({ message: "User created successfully, check your email for verification link.", user: newUser });
+
+    return res.status(201).json({
+      message:
+        "User created successfully, check your email for verification link.",
+      user: newUser,
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return res.status(500).json({ error: "Error occured while creating user" });
   } finally {
-    await prisma.$disconnect();    
+    await prisma.$disconnect();
   }
 };
 
 // verify email
-export const verifyEmail = async (req, res)=>{
+export const verifyEmail = async (req, res) => {
   try {
-    const {token, userId} = req.query;
+    const { token, userId } = req.query;
     // find verification token in the database
     const tokenEntry = await prisma.emailVerification.findFirst({
       where: {
         userId: Number(userId),
-        expireAt: {gte: new Date()}
+        expireAt: { gte: new Date() },
       },
-      orderBy:{
-        expireAt: "desc"
-      }
+      orderBy: {
+        expireAt: "desc",
+      },
     });
-    if (!tokenEntry){
-      return res.status(400).json({ error: 'Invalid token' });
+    if (!tokenEntry) {
+      return res.status(400).json({ error: "Invalid token" });
     }
     // check if token has expired
-    const now = new Date();    
-    if(now > tokenEntry.expireAt){
+    const now = new Date();
+    if (now > tokenEntry.expireAt) {
       return res.status(400).json({ error: "Token has expired" });
     }
     const isTokenValid = await bcrypt.compare(token, tokenEntry.token);
     if (!isTokenValid) {
-      return res.status(400).json({ error: 'Invalid verification token' });
+      return res.status(400).json({ error: "Invalid verification token" });
     }
     // mark as verify user
     await prisma.user.update({
-      where: {id: Number(userId)},
-      data: {isVerified: true}
+      where: { id: Number(userId) },
+      data: { isVerified: true },
     });
     // delete verification token
     await prisma.emailVerification.delete({
-      where: {id: tokenEntry.id}
-    })
-    return res.status(200).json({message: "Email verified successfully"})
+      where: { id: tokenEntry.id },
+    });
+    return res.status(200).json({ message: "Email verified successfully" });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({error: "Error verifying email"});
-  }finally{
+    return res.status(500).json({ error: "Error verifying email" });
+  } finally {
     await prisma.$disconnect();
   }
-}
+};
 
 // login user
 export const loginUser = async (req, res) => {
@@ -129,8 +153,10 @@ export const loginUser = async (req, res) => {
       return res.status(401).json({ error: "Invalid credentials" });
     }
     // check for email verification
-    if (!isUserExists.isVerified){
-      return res.status(403).json({error: "Kindly verify your email before logging in"})
+    if (!isUserExists.isVerified) {
+      return res
+        .status(403)
+        .json({ error: "Kindly verify your email before logging in" });
     }
     const isPasswordCorrect = await bcrypt.compare(
       password,
@@ -140,7 +166,7 @@ export const loginUser = async (req, res) => {
     if (!isPasswordCorrect) {
       return res.status(401).json({ error: "Invalid credentials" });
     } else {
-      req.session.userId  = isUserExists.id;
+      req.session.userId = isUserExists.id;
       return res.status(201).json({ message: "Login successfully" });
     }
   } catch (error) {
@@ -154,57 +180,105 @@ export const loginUser = async (req, res) => {
 };
 
 // update bio
-export const bio = async (req, res)=>{
+export const bio = async (req, res) => {
   try {
-    const {userId} = req.session;
-    const {bio} = req.body;
+    const { userId } = req.session;
+    const { bio } = req.body;
 
-    if (!bio){
-      return res.status(403).json({})
+    if (!bio) {
+      return res.status(403).json({});
     }
     // update user bio
     const updatedProfile = await prisma.profile.upsert({
-      where: {userId: Number(userId)},
-      update: {bio: bio},
+      where: { userId: Number(userId) },
+      update: { bio: bio },
       create: {
         bio: bio,
-        userId: Number(userId)
-      }
+        userId: Number(userId),
+      },
     });
-    return res.status(200).json({message: "Profile updated successfully", updatedProfile});
+    return res
+      .status(200)
+      .json({ message: "Profile updated successfully", updatedProfile });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({error: "Error occur while updating profile"})
-  }finally{
+    return res
+      .status(500)
+      .json({ error: "Error occur while updating profile" });
+  } finally {
     await prisma.$disconnect();
   }
 };
 
+// uploading profile picture
+export const uploadProfilePicture = async (req, res) => {
+  const { userId } = req.session;
+  upload.single("profilePicture")(req, res, async (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error uploading file" });
+    }
+    try {
+      // Check if file is uploaded
+      if (!req.file) {
+        return res.status(400).json({ error: "No file uploaded" });
+      }
+
+      const profilePictureUrl = req.file.path;
+      // save the image url in the database profile table
+      console.log(`User : ${userId}  ProfileURL : ${profilePictureUrl}`);      
+
+      // Check if user profile already exist
+      const isExist = prisma.profile.findFirst({ where: { userId } });
+      if (!isExist)
+        return res.status(404).json({ message: "User profile exist" });
+
+      await prisma.profile.create({ data: { userId, profilePictureUrl } });
+
+      return res.status(200).json({
+        message: "Profile picture uploaded successfully",
+        profilePictureUrl,
+      });
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json({ error: "Error occur while uploading picture" });
+    } finally {
+      await prisma.$disconnect();
+    }
+  });
+};
+
 // delete user account
-export const deleteUserAccount = async (req, res)=>{
+export const deleteUserAccount = async (req, res) => {
   try {
-    const {userId} = req.session;
+    const { userId } = req.session;
     // check if user exists
     const isUserExists = await prisma.user.findUnique({
-      where: {id: Number(userId)}
-    })
-    if (!isUserExists){
-      return res.status(404).json({error: "User not found"});
+      where: { id: Number(userId) },
+    });
+    if (!isUserExists) {
+      return res.status(404).json({ error: "User not found" });
     }
     // delete user account
     await prisma.user.delete({
-      where: {id: Number(userId)}
+      where: { id: Number(userId) },
     });
     // clear user session
     req.session.destroy();
-    return res.status(200).json({message: "User account deleted successfully"});
+    return res
+      .status(200)
+      .json({ message: "User account deleted successfully" });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({error: "Error occur while deleting account"});
-  }finally{
+    return res
+      .status(500)
+      .json({ error: "Error occur while deleting account" });
+  } finally {
     await prisma.$disconnect();
   }
-}
+};
 
 // logout user
 export const logoutUser = (req, res) => {
@@ -220,4 +294,3 @@ export const logoutUser = (req, res) => {
     return res.status(500).json({ error: "Enternal server error" });
   }
 };
-
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {bio, createUser, deleteUserAccount, loginUser, logoutUser, verifyEmail} from "../controllers/index.js";
+import {bio, createUser, deleteUserAccount, loginUser, logoutUser, uploadProfilePicture, verifyEmail} from "../controllers/index.js";
 import {bioSchema, userLoginSchema, userRegistrationSchema} from "../schemas/userSchema.js";
 import { validateRequest } from "../middleware/requestValidate.js";
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post("/register", validateRequest(userRegistrationSchema), createUser);
 router.get("/verify-email", verifyEmail);
 router.post("/login", validateRequest(userLoginSchema), loginUser);
 router.put("/update-bio", validateRequest(bioSchema), bio);
+router.post("/upload-picture", uploadProfilePicture)
 router.post("/logout", logoutUser);
 router.delete("/delete-account", deleteUserAccount);
 
