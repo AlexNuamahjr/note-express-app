94bf55b371b2aa028620ae931aa398464a429ada
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-15 22:03:34 +0000

    update: bin page updated

--- frontend/src/pages/bin/Bin.jsx
+++ frontend/src/pages/bin/Bin.jsx
@@ -1,27 +1,28 @@
-import React, { useState } from 'react';
-import Sidebar from '../../components/Sidebar';
+import React, { useState } from "react";
+import Layout from "../../components/Layout";
 
 const RecycleBin = () => {
   const [deletedNotes, setDeletedNotes] = useState([]);
 
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="ml-64 p-4 w-full">
-        <h1 className="text-3xl font-bold mb-4">Recycle Bin</h1>
-        <div>
-          {deletedNotes.length === 0 ? (
-            <p>No deleted notes.</p>
-          ) : (
-            deletedNotes.map((note) => (
-              <div key={id} className="p-4 bg-red-100 mb-4 rounded shadow">
-                <div dangerouslySetInnerHTML={{ __html: note }} />
-              </div>
-            ))
-          )}
+    <Layout>
+      <div className="flex">        
+        <div className="ml-64 p-4 w-full">
+          <h1 className="text-3xl font-bold mb-4">Recycle Bin</h1>
+          <div>
+            {deletedNotes.length === 0 ? (
+              <p className="mx-auto">No deleted notes.</p>
+            ) : (
+              deletedNotes.map((note) => (
+                <div key={id} className="p-4 bg-red-100 mb-4 rounded shadow">
+                  <div dangerouslySetInnerHTML={{ __html: note }} />
+                </div>
+              ))
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
