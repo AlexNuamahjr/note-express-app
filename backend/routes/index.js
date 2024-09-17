e98c3166e2f3838822f992f9804704f4575956d8
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-17 23:47:58 +0000

    prisma schema updated

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,12 +3,11 @@ import {bio, createUser, deleteUserAccount, loginUser, logoutUser, uploadProfile
 import {bioSchema, userLoginSchema, userRegistrationSchema} from "../schemas/validationSchema.js";
 import { validateRequest } from "../middleware/requestValidate.js";
 import { isAuthenticated } from "../middleware/authenticate.js";
-import { limiter } from "../middleware/rateLimit.js";
 const router = express.Router();
 
 router.post("/register", validateRequest(userRegistrationSchema), createUser);
 router.get("/verify-email", verifyEmail);
-router.post("/login", limiter, validateRequest(userLoginSchema), loginUser);
+router.post("/login", validateRequest(userLoginSchema), loginUser);
 router.put("/update-bio", isAuthenticated, validateRequest(bioSchema), bio);
 router.post("/upload-picture", isAuthenticated, uploadProfilePicture)
 router.post("/logout", isAuthenticated, logoutUser);
