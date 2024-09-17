98c06294e98e7e6e9909b5e567bc147aa1720e73
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-17 06:25:36 +0000

    routes set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import Header from "./components/Header";
-import { BrowserRouter, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import Login from "./pages/Auth/login/Login";
 import Register from "./pages/Auth/signup/Signup";
 import Home from "./pages/home/Home";
 import RecycleBin from "./pages/bin/Bin";
 import ResetPassword from "./pages/Auth/resetPassword/ResetPassword";
-import Layout from "./components/Layout";
-import ErrorPage from "./pages/error/404";
 import EditProfile from "./pages/profile/Profile";
 import ChangePassword from "./pages/Auth/changePassword/changePassword";
 import ForgotPassword from "./pages/Auth/forgotPassword/ForgotPassword";
@@ -16,8 +18,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/bin" Component={RecycleBin} />
+        <Route path="/" element={<Home />} />
+        <Route path="/bin" element={<RecycleBin />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/profile" element={<EditProfile />} />
+        <Route path="/change-password" element={<ChangePassword />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   );
