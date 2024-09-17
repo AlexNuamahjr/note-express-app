ef602c0dc00f0375cf8409426f6d3775a883a483
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-17 06:14:49 +0000

    update: refactored

--- backend/middleware/requestValidate.js
+++ backend/middleware/requestValidate.js
@@ -1,16 +1,17 @@
+export const validateRequest = (schema) => {
+  return (req, res, next) => {
+    try {
+      const { error } = schema.validate(req.body, { abortEarly: false });
+      console.log(error);
 
-export const validateRequest = (schema)=>{
-    return (req, res, next)=>{
-        try {
-            // console.log("validate request",req.body);
-            
-            const {error} = schema.validate(req.body, {abortEarly: false});
-            if (error){
-                return res.status(400).json({ errors: error.details.map(err => err.message)});
-            }
-            next();
-        } catch (error) {
-            return next(error);
-        }
+      if (error) {
+        return res
+          .status(400)
+          .json({ errors: error.details.map((err) => err.message) });
+      }
+      next();
+    } catch (error) {
+      return next(error);
     }
-}
+  };
+};
