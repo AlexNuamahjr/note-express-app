bf6213f9847278faeb2dec8e7e0c9f95e175f63d
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-17 06:28:38 +0000

    validation schema

---
+++ frontend/src/utils/validationSchema.js
@@ -0,0 +1,17 @@
+import * as yup from "yup";
+
+export const registrationValidationSchema = yup.object().shape({
+  firstName: yup.string().required("first name is required"),
+  lastName: yup.string().required("Last name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  userName: yup.string().required("Username is required"),
+  gender: yup.string().required("Gender is required"),
+  phoneNumber: yup.string().required("Phone number is required"),
+  dob: yup.date().required("Date of birth is required"),
+  password: yup.string().required("Password is required"),
+});
+
+export const loginValidationSchema = yup.object().shape({
+  email: yup.string().required("Email is required"),
+  password: yup.string().required("Password is required"),
+});
