95421995ebbeed2de7ed7e2073443829117f6a9f
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-20 17:07:04 +0000

    update: route protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Auth/login/Login";
 import Register from "./pages/Auth/signup/Signup";
 import Home from "./pages/home/Home";
@@ -14,23 +10,27 @@ import ForgotPassword from "./pages/Auth/forgotPassword/ForgotPassword";
 import Settings from "./pages/settings/Settings";
 import EmailConfirmation from "./pages/Auth/emailComfirmation/emailComfirmation";
 import EmailVerification from "./pages/Auth/emailVerification/emailVerification";
+import { AuthProvider } from "./utils/auth";
+import ProtectedRoute from "./utils/protectRoutes";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/bin" element={<RecycleBin />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/profile" element={<EditProfile />} />
-        <Route path="/change-password" element={<ChangePassword />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/email-confirmation" element={<EmailConfirmation />} />
-        <Route path="/verify-email" element={<EmailVerification />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/bin" element={<ProtectedRoute><RecycleBin /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute><EditProfile /></ProtectedRoute>} />
+          <Route path="/change-password" element={<ProtectedRoute><ChangePassword /></ProtectedRoute>} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/email-confirmation" element={<EmailConfirmation />} />
+          <Route path="/verify-email" element={<EmailVerification />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
