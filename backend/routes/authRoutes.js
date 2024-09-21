747700f172b4baef2be761a98f85ad9ef6c8d667
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-21 23:57:26 +0000

    update: check session route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {checkSession, forgotPassword, resetPassword} from "../controllers/authController.js";
+import {forgotPassword, resetPassword} from "../controllers/authController.js";
 import {changePasswordSchema, forgotPasswordSchema, resetPasswordSchema} from "../schemas/validationSchema.js";
 import { validateRequest } from "../middleware/requestValidate.js";
 import { isAuthenticated } from "../middleware/authenticate.js";
@@ -7,7 +7,6 @@ import { changePassword } from "../controllers/changePassword.js";
 
 const authRouter = express.Router();
 
-authRouter.get("/check-session", checkSession);
 authRouter.post("/forgot-password", isAuthenticated, validateRequest(forgotPasswordSchema), forgotPassword);
 authRouter.post("/reset-password", isAuthenticated, validateRequest(resetPasswordSchema), resetPassword);
 authRouter.patch("/change-password", isAuthenticated,  validateRequest(changePasswordSchema), changePassword);
