fa9edad9f9c51f43090368a9738891d60eed97d9
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-24 00:04:40 +0000

    update: add note button

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,43 +1,44 @@
-import React, { useState } from 'react';
-import QuillEditor from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import Layout from '../../components/Layout';
+import React, { useState } from "react";
+import QuillEditor from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import Layout from "../../components/Layout";
+import NotesGrid from "../../components/Notes";
 
 const Home = () => {
-  const [note, setNote] = useState('');
+  const [note, setNote] = useState({ title: "", content: " " });
   const [notes, setNotes] = useState([]);
 
   const addNote = () => {
     setNotes([...notes, note]);
-    setNote('');
+    setNote({ title: "", content: "" });
   };
 
   return (
     <Layout>
-    <div className="flex-1 mx-auto">      
-       {/* Main content area */}
-      <div className="flex-1 p-4 overflow-auto">
-        <div className="max-w-4xl mx-auto">
-          <h1 className="text-2xl font-bold mb-6">Add a Note</h1>
-          <div className="bg-white p-4 shadow-md rounded-md">
-            {/* Quill Editor */}
-            <QuillEditor />
-          </div>
-
-          {/* Notes Display Section */}
-          <div className="mt-8">
-            <h2 className="text-xl font-semibold mb-4">Your Notes</h2>
-            <div className="space-y-4">
-              {/*note card */}
-              <div className="p-4 bg-gray-100 shadow-md rounded-md">
-                This is an example note.
-              </div>
-              
+      <div className="flex-1 mx-auto">
+        {/* Main content area */}
+        <div className="flex-1 p-4 overflow-auto">
+          <div className="max-w-4xl mx-auto">
+            <h1 className="text-2xl font-bold mb-6">Add a Note</h1>
+            <div className="bg-white p-4 shadow-md rounded-md">
+              {/* Quill Editor */}
+              <QuillEditor
+                value={note.content}
+                onChange={(content) => setNote({ ...note, content })}
+              />
+              <button
+                className="mt-4 p-2 bg-blue-600 text-white rounded"
+                onClick={addNote}
+              >
+                Add Note
+              </button>
             </div>
+
+            {/* Notes Display Section */}
+            <NotesGrid notes={note} />
           </div>
         </div>
       </div>
-    </div>
     </Layout>
   );
 };
