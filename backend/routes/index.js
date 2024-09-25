90584e6c6683fbc5222c7b45e0e03d6cc759d2ea
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-25 22:21:35 +0000

    update: check-session route omited

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,18 @@
 import express from "express";
-import {bio, checkSession, createUser, deleteUserAccount, loginUser, logoutUser, uploadProfilePicture, verifyEmail} from "../controllers/index.js";
-import {bioSchema, userLoginSchema, userRegistrationSchema} from "../schemas/validationSchema.js";
+import {
+  bio,
+  createUser,
+  deleteUserAccount,
+  loginUser,
+  logoutUser,
+  uploadProfilePicture,
+  verifyEmail,
+} from "../controllers/index.js";
+import {
+  bioSchema,
+  userLoginSchema,
+  userRegistrationSchema,
+} from "../schemas/validationSchema.js";
 import { validateRequest } from "../middleware/requestValidate.js";
 import { isAuthenticated } from "../middleware/authenticate.js";
 const router = express.Router();
@@ -8,9 +20,8 @@ const router = express.Router();
 router.post("/register", validateRequest(userRegistrationSchema), createUser);
 router.get("/verify-email", verifyEmail);
 router.post("/login", validateRequest(userLoginSchema), loginUser);
-router.get("/check-session", checkSession)
 router.put("/update-bio", isAuthenticated, validateRequest(bioSchema), bio);
-router.post("/upload-picture", isAuthenticated, uploadProfilePicture)
+router.post("/upload-picture", isAuthenticated, uploadProfilePicture);
 router.post("/logout", isAuthenticated, logoutUser);
 router.delete("/delete-account", isAuthenticated, deleteUserAccount);
 
