14af89f61f8a95335f264e0f285271ab2d5607f3
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-25 21:51:26 +0000

    auth middleware updated from express-session to jwt

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -1,8 +1,36 @@
-export const isAuthenticated = (req, res, next)=>{
-    if (req.session.userId){
-        return next();
-    }else{
-        return res.status(403).json({message: "Kindly login to proceed"})
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const isAuthenticated = asyncHandler(async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.status(401).json({ message: "Kindly login to proceed" });
     }
-    
-}
+
+    // Verify the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Find the user based on decoded token information
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.userId },
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+
+    return res.status(401).json({ message: "Kindly login to proceed" });
+  } finally {
+    await prisma.$disconnect();
+  }
+});
