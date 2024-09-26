077957e073c110e170e8a6639825ec5c35320673
Alexander Nuamah Jr <alexandernuamah@gmail.com>
2024-09-26 23:03:22 +0000

    route protection

--- frontend/src/utils/protectRoutes.js
+++ frontend/src/utils/protectRoutes.js
@@ -5,14 +5,15 @@ import { useEffect } from "react";
 const ProtectedRoute = ({ Children }) => {
   const navigate = useNavigate();
   const { isAuthenticated } = useAuth();
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      return navigate("/login");
-    }
-  }, [isAuthenticated, navigate]);
-  //   if user is not authenticated
-  if (!isAuthenticated) return null;
+  console.log(isAuthenticated);
+  
+  // useEffect(() => {
+  //   if (!isAuthenticated) {
+  //     return navigate("/");
+  //   }
+  // }, [isAuthenticated, navigate]);
+  // //   if user is not authenticated
+  // if (!isAuthenticated) return null;
   // if user is authenticated
   return Children;
 };
